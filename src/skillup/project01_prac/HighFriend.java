77aa0117fb91595579bbb35a6fe4ba10e9bf3a37
likewhat9901 <parkko123@naver.com>
2025-03-28 18:31:27 +0900

    .

---
+++ src/skillup/project01_prac/HighFriend.java
@@ -0,0 +1,16 @@
+package skillup.project01_prac;
+
+public class HighFriend {
+	
+	String name;
+	String phone;
+	String addr;
+	String nickname;
+	
+	public void showAllData() {
+		System.out.println("고딩친구[전체정보]");
+	}
+	public void showBasicInfo() {
+		System.out.println("고딩친구[간략정보]");
+	}
+}
---
+++ src/skillup/project01_prac/UnivFriend.java
@@ -0,0 +1,16 @@
+package skillup.project01_prac;
+
+public class UnivFriend {
+
+	String name;
+	String phone;
+	String addr;
+	String major;
+	
+	public void showAllData() {
+		System.out.println("대딩친구[전체정보]");
+	}
+	public void showBasicInfo() {
+		System.out.println("대딩친구[간략정보]");
+	}
+}
